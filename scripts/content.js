6741898fcef5cac13d65b6c6d332ab92f5abf099
Jamie Kirkwin <jkirkwin@gmail.com>
2024-03-06 09:55:53 -0800

    Add unstyled link to letterboxd search
    When a film is detected in the search results, add a new section with
    a vanilla link to letterboxd. This will need to be styled properly.

--- scripts/content.js
+++ scripts/content.js
@@ -12,24 +12,41 @@ function error(message) {
     console.error(extensionLogPrefix + message)
 }
 
-debug("Content script running");
+function tryAddFilmInfo() {
+    const watchMovieDiv = document.querySelector('div[aria-label="Watch movie"]');
+    if (watchMovieDiv) {
+        log("Detected a film in search results");
 
-const watchMovieDiv = document.querySelector('div[aria-label="Watch movie"]');
-if (watchMovieDiv) {
-    log("Detected a film in search results");
+        const filmTitle = document.querySelector('div[aria-level="2"][data-attrid="title"][role="heading"]')?.innerHTML
+        if (!filmTitle) {
+            error("Failed to find film title")
+            return
+        }
 
-    const title = document.querySelector('div[aria-level="2"][data-attrid="title"][role="heading"]')?.innerHTML
-    if (!title) {
-        error("Failed to find film title")
+        log("Adding letterboxd search link");
+        const contentContainer = document.createElement('div');
+
+        // Pull the "Watch movie" heading from the page and copy it to get a consistently-styled heading
+        // for the letterboxd section. They've just used div's everywhere (i.e. no h1/h2/etc.), so we'll do the same.
+        const sampleHeading = watchMovieDiv.querySelector('div[aria-level="2"][role="heading"]');
+        const sampleHeadingContainer = sampleHeading.parentElement
+        const headingContainer = sampleHeadingContainer.cloneNode(true)
+        headingContainer.firstChild.textContent = 'Letterboxd'
+
+        contentContainer.appendChild(headingContainer)
+
+        // Add a link to search for the film on letterboxd
+        // TODO Make this pretty
+        const link = document.createElement('a')
+        link.href = `https://letterboxd.com/search/films/${filmTitle}/`;
+        link.textContent = 'Search on letterboxd'
+        contentContainer.appendChild(link)
+
+        watchMovieDiv.insertAdjacentElement('afterbegin', contentContainer);
+
+    } else {
+        log("Film panel not found");
     }
-
-    const directorName = document.querySelector('div[data-attrid="kc:/film/film:director"] a')?.innerHTML
-    if (!directorName) {
-        log("Unable to find director")
-    }
-
-    log(`'${title}' by ${directorName}`)
-
-} else {
-    log("Film panels not found");
 }
+
+tryAddFilmInfo()
