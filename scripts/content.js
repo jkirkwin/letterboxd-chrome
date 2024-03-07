9fb354ad315791f46c3c8012534607e11044a857
Jamie Kirkwin <jkirkwin@gmail.com>
2024-03-06 18:13:40 -0800

    Improve link formatting, include logo
    Add reasonably well-styled letterboxd logo and prompt text as a
    clickable link above the "Watch movie" section.

--- scripts/content.js
+++ scripts/content.js
@@ -23,30 +23,54 @@ function tryAddFilmInfo() {
             return
         }
 
-        log("Adding letterboxd search link");
-        const contentContainer = document.createElement('div');
-
-        // Pull the "Watch movie" heading from the page and copy it to get a consistently-styled heading
-        // for the letterboxd section. They've just used div's everywhere (i.e. no h1/h2/etc.), so we'll do the same.
-        const sampleHeading = watchMovieDiv.querySelector('div[aria-level="2"][role="heading"]');
-        const sampleHeadingContainer = sampleHeading.parentElement
-        const headingContainer = sampleHeadingContainer.cloneNode(true)
-        headingContainer.firstChild.textContent = 'Letterboxd'
-
-        contentContainer.appendChild(headingContainer)
-
-        // Add a link to search for the film on letterboxd
-        // TODO Make this pretty
-        const link = document.createElement('a')
-        link.href = `https://letterboxd.com/search/films/${filmTitle}/`;
-        link.textContent = 'Search on letterboxd'
-        contentContainer.appendChild(link)
-
-        watchMovieDiv.insertAdjacentElement('afterbegin', contentContainer);
+        const letterboxdLinkContainer = buildLetterboxdSearchSection(watchMovieDiv, filmTitle)
+        watchMovieDiv.insertAdjacentElement('afterbegin', letterboxdLinkContainer);
 
     } else {
         log("Film panel not found");
     }
+}
+
+/*
+ * Build and return an element holding a link to search for the film on letterboxd.
+ * Takes the div holding the "watch movie" links to reuse the styling of its contents
+ * and the title of the film to search.
+ */
+function buildLetterboxdSearchSection(watchMovieDiv, filmTitle) {
+    // TODO Add horizontal divider between new section and "Watch movie"
+
+    const contentContainer = document.createElement('div');
+
+    // Pull the "Watch movie" heading from the page and copy it to get a consistently-styled heading
+    // for the letterboxd section. They've just used div's everywhere (i.e. no h1/h2/etc.), so we'll do the same.
+    const sampleHeading = watchMovieDiv.querySelector('div[aria-level="2"][role="heading"]');
+    const sampleHeadingContainer = sampleHeading.parentElement
+    const headingContainer = sampleHeadingContainer.cloneNode(true)
+    headingContainer.firstChild.textContent = 'Letterboxd'
+    contentContainer.appendChild(headingContainer)
+
+    // Add a link to search for the film on letterboxd
+    const linkContainer = document.createElement('a')
+    linkContainer.href = `https://letterboxd.com/search/films/${filmTitle}/`
+    linkContainer.style.color = 'inherit'
+    linkContainer.style.display = 'flex'
+    linkContainer.style.alignItems = 'center'
+    linkContainer.style.gap = '10px'
+    linkContainer.style.padding = '10px 5px'
+
+    const logoImage = document.createElement('img')
+    logoImage.src = chrome.runtime.getURL('assets/letterboxd_logo.png')
+    logoImage.height = '45'
+    logoImage.width = '45'
+    linkContainer.appendChild(logoImage)
+
+    const linkText = document.createElement('span')
+    linkText.textContent = 'View on Letterboxd'
+    linkContainer.appendChild(linkText)
+
+    contentContainer.appendChild(linkContainer)
+
+    return contentContainer
 }
 
 tryAddFilmInfo()
