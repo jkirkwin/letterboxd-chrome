abe519ad5b0b334253a38851ebb92ea726876533
Jamie Kirkwin <jkirkwin@gmail.com>
2024-03-06 22:02:04 -0800

    Initial setup
    Add boilerplate code for the extension and implement a simple button to link to a letterboxd search for a film.

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # letterboxd-chrome
-Browser extension to replace the watchlist actions available in chrome to map to letterboxd instead.
+A simple chrome extension to add functionality to google's film search results.
+
+## How it works
+When a user searches for a film, google provides extra panels with links to watch the movie on different streaming services, to mark the film as watched, add it to the user's watchlist, etc. The extension monitors the page structure, detects when these panels are present, and inserts utilities into the page.
+
+## Features
+Adds a quick link to search for the same film on letterboxd.
+
+## API Access
+
+In the future I would like to add support for marking a film as watched, rating a film, and adding/removing a film from the user's watchlist. Google's existing features for this are not very useful: users can't access a list of the movies they've marked as watched, and the watchlist as capped at 96 films, after which it stops displaying the older entries.
+
+ Implementing this would require access to the [letterboxd API](https://letterboxd.com/api-beta/) which is still in closed beta at the time of writing. I've requested access to the API via email, but haven't heard back. If you'd like to see this feature, please feel free to send them an email as well.
---
+++ scripts/content.js
@@ -0,0 +1,91 @@
+const extensionLogPrefix = "letterboxd-chrome: "
+
+function log(message) {
+    console.log(extensionLogPrefix + message)
+}
+
+function debug(message) {
+    console.debug(extensionLogPrefix + message)
+}
+
+function error(message) {
+    console.error(extensionLogPrefix + message)
+}
+
+function tryAddFilmInfo() {
+    // TODO This won't work for obscure movies that don't have any watch options. 
+    // E.g. https://www.google.com/search?q=chaos+DeFalco, https://www.google.com/search?q=four+lions
+    // Find a backup option to use to get formatting and detect that the user did in fact search a movie.
+    const watchMovieDiv = document.querySelector('div[aria-label="Watch movie"]');
+    if (watchMovieDiv) {
+        log("Detected a film in search results");
+
+        const filmTitle = document.querySelector('div[aria-level="2"][data-attrid="title"][role="heading"]')?.innerHTML
+        if (!filmTitle) {
+            error("Failed to find film title")
+            return
+        }
+
+        const letterboxdLinkContainer = buildLetterboxdSearchSection(watchMovieDiv, filmTitle)
+        watchMovieDiv.insertAdjacentElement('afterbegin', letterboxdLinkContainer);
+
+    } else {
+        log("Film panel not found");
+    }
+}
+
+/*
+ * Build and return an element holding a link to search for the film on letterboxd.
+ * Takes the div holding the "watch movie" links to reuse the styling of its contents
+ * and the title of the film to search.
+ */
+function buildLetterboxdSearchSection(watchMovieDiv, filmTitle) {
+    const contentContainer = document.createElement('div');
+    contentContainer.style.padding = '0 0 10px 0'
+
+    // Pull the "Watch movie" heading from the page and copy it to get a consistently-styled heading
+    // for the letterboxd section. They've just used div's everywhere (i.e. no h1/h2/etc.), so we'll do the same.
+    const sampleHeading = watchMovieDiv.querySelector('div[aria-level="2"][role="heading"]');
+    const sampleHeadingContainer = sampleHeading.parentElement
+    const headingContainer = sampleHeadingContainer.cloneNode(true)
+    headingContainer.style.padding = '0 0 10px 0'
+    headingContainer.firstChild.textContent = 'Letterboxd'
+    contentContainer.appendChild(headingContainer)
+
+    // Add a link to search for the film on letterboxd
+    const linkContainer = document.createElement('a')
+    // TODO May be able to link directly to the letterboxd film page. 
+    // In most cases the url is just the film name with spaces subbed for hyphens, and in others they include the year.
+    // It seems like we could add the year every time and let their redirects handle it.
+    // If there are a few edge cases where this won't work, can try sending an https request to the film url we want to try,
+    // and fall back to using this search link if it fails.
+    linkContainer.href = `https://letterboxd.com/search/films/${filmTitle}/`
+    linkContainer.style.color = 'inherit'
+    linkContainer.style.display = 'flex'
+    linkContainer.style.alignItems = 'center'
+    linkContainer.style.gap = '10px'
+
+    const logoImage = document.createElement('img')
+    logoImage.src = chrome.runtime.getURL('assets/letterboxd_logo.png')
+    logoImage.height = '45'
+    logoImage.width = '45'
+    linkContainer.appendChild(logoImage)
+
+    const linkText = document.createElement('span')
+    linkText.textContent = 'View on Letterboxd'
+    linkContainer.appendChild(linkText)
+
+    contentContainer.appendChild(linkContainer)
+
+    // Add a horizontal rule below the new content
+    const separator = watchMovieDiv.querySelector('[role="separator"]')?.cloneNode() ?? document.createElement('hr')
+    contentContainer.appendChild(separator)
+
+    return contentContainer
+}
+
+// TODO When on a page like https://www.google.com/search?sa=X&sca_esv=eab7f0f30e16ba03&biw=1032&bih=738&sxsrf=ACQVn0_7ZiyZ8GWMzOP0CiSwAM5WzLRugA:1709789662998&si=AKbGX_oBDfquzodaRrfbb9img4kPQ4fCBZjeqAiaW1svvC8uXheAX-kHFU4c0bex6eNeXQ3iMkESrR-67uRmjGFFyrLdv_w9jpayDPg1ABrWxypVf4IngX5g3x7JDvW-5CunMxL5RJrp8xtX21LUmFr4PScuFW2f5I3DsvCBRUfAbU31bsoXfaEYYDbznqcvbUhIdIyBOaNJX7_T9pH8QLb5Tb_gVw7XatNWykV5MM0CY5QNkXLVn287HADS5x3HxlLsAkHxP8Nn0l_Qb52csV7OK2G3s6y79A%3D%3D&q=Once+Upon+a+Time+in+America&lei=5FHpZYb4CoK80PEPofmMgAk
+// where the user can switch between movies, we need to redo the link generation process. Likely we can just detect changes
+// to the search query and/or url.
+
+tryAddFilmInfo()
