66cf745d088b3acf66aed06249251b50c21e5bd5
Jamie Kirkwin <jkirkwin@gmail.com>
2024-03-06 21:19:32 -0800

    Create basic popup content
    Add a simple popup with a link to the github repo. Remove unused
    js file.

--- popup.js
+++
@@ -1,3 +0,0 @@
-console.log("This is a popup!");
-// TODO Make something useful or remove. 
-// Might want to use the popup to have the user log into letterboxd/do whatever auth is necessary to use the letterboxd apis
